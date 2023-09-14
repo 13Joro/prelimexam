84dce3756bc61c0243c7c957978db5d045f46aa3
死Joro <119878704+13Joro@users.noreply.github.com>
2023-09-14 10:47:49 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I AM A STUDENT FROM UNIVERSITY OF THE ASSUMPTION</p>
-        <p>I&#39;m Kervin Amiel Josh K. Canlas, but you can call me &quot;Joro.&quot; I enjoy playing online games with my
-friends, and I really like the song &quot;And&quot; by Eden, even though not many people know about it. My top
-foods are Shawarma and Pineapple Pizza. I don&#39;t have a lot of social events, but what I love is the fun we
-have when we game together. It&#39;s a different kind of fun, unlike always going out or partying.</p>
+        <p>10 Things That Require Zero TalentN</p>
+    <ul>
+      <li>1. Being On Time.</li>
+      <li>2. Making An Effort.</li>
+      <li>3. Being High Energy.</li>
+      <li>4. Having A Positive Attitude.</li>
+      <li>5. Being Passionate.</li>
+      <li>6. Using Good body Language.</li>
+      <li>7. Being Caochable.</li>
+      <li>8. Doing A Little Extra.</li>
+      <li>9. Being Prepared.</li>
+      <li>10. Having A Strong Work Ethic.</li>
+  </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
